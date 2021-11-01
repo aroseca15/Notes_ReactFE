68e3546de48f13e7bcea12af27fc63d5933c0ba5
Your Name <aroseca15@gmail.com>
2021-11-01 15:59:26 -0700

    getting an error at update individ. note

--- notes_reactfe/src/pages/DetailsCard.js
+++ notes_reactfe/src/pages/DetailsCard.js
@@ -4,7 +4,11 @@ import { useEffect, useState } from 'react';
 // import axios from 'axios';
 
 
-function DetailsCard({ match }) {
+function DetailsCard({ match, history }) {
+    const [showTextBox, setShowTextBox] = useState(false);
+    const onClickTB = () => setShowTextBox(!showTextBox);
+    // const [showSaveBtn, setShowSaveBtn] = useState(false);
+    // const onClickSB = () => setShowSaveBtn(!showSaveBtn);
     let noteId = match.params.id;
     const notesBaseURL = `http://localhost:3001/notes/${noteId}`
     // let note = notes.find(note => note.id === Number(noteId))
@@ -22,20 +26,44 @@ function DetailsCard({ match }) {
         setNote(data);
     }
 
+    let updateNote = async () => {
+        await fetch(notesBaseURL, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            note: JSON.stringify({ ...note, 'updated': new Date() })
+        })
+    }
 
+    let handleSubmit = () => {
+        updateNote()
+        history.push('/notes')
+    }
 
 
     return (
-        <div className="card" style={{ width: "18rem;" }}>
-            <div className="card-body">
-                <h5 className="card-title">{note?.patient}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{note?.date}, {note?.time}</h6>
-                <p className="card-text">{note?.note}</p>
-                <a href="#" className="card-link">Prev Note</a>
-                <a href="#" className="card-link">Next Note</a>
-                <a href="#" className="card-link">Close</a>
+        <>
+            <div className="card" style={{ width: "18rem;" }}>
+                <div className="card-body">
+                    <button onClick={onClickTB}>Edit Note</button>
+                    <h5 className="card-title">{note?.patient}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{note?.date}, {note?.time}</h6>
+                    <p className="card-text">{note?.note}</p>
+                    {/* { showSaveBtn ?  : null } */}
+                    <a href="#" className="card-link">Prev Note</a>
+                    <a href="#" className="card-link">Next Note</a>
+                    <a href="#" className="card-link">Close</a>
+                </div>
             </div>
-        </div>
+
+            {showTextBox ? <>
+                <textarea onChange={(e) => { setNote(...note, { 'note': e.target.value })}} value={note?.note}></textarea>
+                <div>
+                    <button onClick={handleSubmit}>Save</button>
+                </div>
+            </> : null}
+        </>
     )
 }
 
